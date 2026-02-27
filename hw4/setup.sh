f4017e72130933950352619674165669ef130ebc
Priya Bajaria <priya02@bu.edu>
2026-02-27 10:08:11 -0500

    Update startup and setup scripts

--- hw4/setup.sh
+++ hw4/setup.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+exec > /var/log/startup.log 2>&1
 
-PROJECT_ID="evident-gecko-486418-c7"
-ZONE="us-central1-a"
-SA_NAME="hw4-sa"
-VM_NAME="hw4-server"
+# Install Python and Git
+apt-get update
+apt-get install -y python3 python3-pip git
 
-echo "=== Creating VM ==="
-gcloud compute instances create $VM_NAME --project=$PROJECT_ID --zone=$ZONE --machine-type="e2-micro" --service-account="$SA_NAME@$PROJECT_ID.iam.gserviceaccount.com" --scopes="cloud-platform" --tags="hw4-server" --image-family="debian-12" --image-project="debian-cloud" --boot-disk-size="10GB" --metadata-from-file=startup-script=startup.sh
+# Clone the repo
+git clone -b hw4 https://github.com/priya02-11/cs528-hw2.git /home/priya/cs528-hw2
 
-echo "=== Done! ==="
+# Go to correct directory
+cd /home/priya/cs528-hw2/hw4
+
+# Install requirements
+pip3 install -r requirements.txt --break-system-packages
+
+# Run the FastAPI server
+nohup /home/priya/.local/bin/uvicorn main:app --host 0.0.0.0 --port 8080 > /var/log/hw4-server.log 2>&1 &
